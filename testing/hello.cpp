1534657bc6c4965405a11025ebbcb0e72365ba2c
csc103 student <me@citymail.ccny.cuny.edu>
2016-09-05 19:14:17 -0400

    test2

--- testing/hello.cpp
+++ testing/hello.cpp
@@ -5,9 +5,10 @@ using std::cout;
 int main()
 {
 	//print
-	cout << "I like git\n";
+	cout << "I like git?\ntruly?";
 	return 0;
 }
 
 // vim:foldlevel=1
 //i wonder what this comment means
+//something to do with vim i guess
